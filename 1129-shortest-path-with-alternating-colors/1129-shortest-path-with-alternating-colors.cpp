eed3ca64d91f63c292a205a5d04a1a1f7b5021e5
Youssif-Asaad-Sobhy <103506012+Youssif-Asaad-Sobhy@users.noreply.github.com>
2023-02-11 12:36:26 +0200

    Time: 138 ms (5.12%), Space: 46.2 MB (5.11%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
-    int x;
+    vector<vector<int>>adjr,adjb;
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
         vector<int>ans;
-        vector<vector<int>>adjr(n),adjb(n);
+        adjr=adjb=vector<vector<int>>(n);
         for(auto it:redEdges)adjr[it[0]].push_back(it[1]);
         for(auto it:blueEdges)adjb[it[0]].push_back(it[1]);
         for(int i=0;i<n;i++)
         {
-            vector<vector<int>>vis(n,vector<int>(2,1000));
-            x=i;
-            int cnt=dfs(0,0,0,adjr,adjb,vis);
-            vis=vector<vector<int>>(n,vector<int>(2,1000));
-            cnt=min(cnt,dfs(0,1,0,adjr,adjb,vis));
-            ans.push_back(cnt==1000?-1:cnt);
+            ans.push_back(bfs(i));
         }
         return ans;
     }
-    int dfs(int i,bool state,int depth,vector<vector<int>>&adjr,vector<vector<int>>&adjb,vector<vector<int>>&vis)
+    int bfs(int ed)
     {
-        if(vis[i][state]<depth)return 1000;
-        if(i==x)return 0;
-        vis[i][state]=depth;
-        int cnt=1000;
-        for(auto it:(state?adjr[i]:adjb[i]))
+        queue<pair<int,bool>>q;
+        q.push({0,0});
+        q.push({0,1});
+        vector<vector<int>>dist(adjr.size(),vector<int>(2,1000));
+        dist[0][0]=0;
+        dist[0][1]=0;
+        while(q.size())
         {
-            if(vis[it][!state]>depth+1)
+            int node=q.front().first;
+            int state=q.front().second;
+            int dis=dist[node][state];
+            q.pop();
+            if(node==ed)return dis;
+            for(auto child:(state?adjr[node]:adjb[node]))
             {
-                cnt=min(1+dfs(it,!state,depth+1,adjr,adjb,vis),cnt);
+                if(dist[child][!state]>dis+1)
+                {
+                    dist[child][!state]=dis+1;
+                    q.push({child,!state});
+                }
             }
         }
-        return cnt;
+        return -1;
     }
 };
