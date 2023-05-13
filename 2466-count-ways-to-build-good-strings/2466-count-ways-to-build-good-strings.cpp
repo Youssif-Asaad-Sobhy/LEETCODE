0f694700e3bc847ffa1cda2057f2b55f75cd74d2
Youssif-Asaad-Sobhy <103506012+Youssif-Asaad-Sobhy@users.noreply.github.com>
2023-05-13 13:02:12 +0300

    Time: 8 ms (94.56%), Space: 6.3 MB (100.00%) - LeetHub

---
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countGoodStrings(int low, int high, int zero, int one) {
+        int dp[100005]={};
+        dp[0]=1;
+        int ans=0,mod=1e9+7;
+
+        for(int i=0;i<=high;i++)
+        {
+            if(dp[i])
+            {
+                if(i+zero<=high)dp[i+zero]+=dp[i],dp[i+zero]%=mod;
+                if(i+one<=high)dp[i+one]+=dp[i],dp[i+one]%=mod;
+            }
+            if(i>=low)ans+=dp[i];
+            ans%=mod;
+        }
+        return ans;
+    }
+};
