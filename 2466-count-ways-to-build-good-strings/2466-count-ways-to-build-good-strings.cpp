3d2a9ac4f72a257211984ea05aee545310354271
Youssif-Asaad-Sobhy <103506012+Youssif-Asaad-Sobhy@users.noreply.github.com>
2023-05-13 13:03:42 +0300

    Time: 8 ms (94.56%), Space: 9.3 MB (81.95%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        int dp[100005]={};
+        vector<int>dp(high+5);
         dp[0]=1;
         int ans=0,mod=1e9+7;
         for(int i=0;i<=high;i++)
@@ -11,8 +11,7 @@ public:
                 if(i+zero<=high)dp[i+zero]+=dp[i],dp[i+zero]%=mod;
                 if(i+one<=high)dp[i+one]+=dp[i],dp[i+one]%=mod;
             }
-            if(i>=low)ans+=dp[i];
-            ans%=mod;
+            if(i>=low)ans+=dp[i],ans%=mod;
         }
         return ans;
     }
